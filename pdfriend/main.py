824a3bf3f6b485d4accd23cfc20403aaecf89604
RayOfSunDull <apollon.grigoras@gmail.com>
2023-12-17 01:48:50 +0200

    added commands to main

--- pdfriend/main.py
+++ pdfriend/main.py
@@ -3,19 +3,26 @@ from pdfriend.classes.platforms import Platform
 import pdfriend.commands as commands
 
 
-parser = argparse.ArgumentParser(add_help=False)
+def validate_arg_num(command_name: str, expected_nargs: int, given_nargs: int):
+    # I allow passing more arguments than expected and just ignore the extra ones
+    if given_nargs == expected_nargs:
+        return
+    print(
+        f"{command_name} command takes {expected_nargs} arguments, {given_nargs} given"
+    )
 
-parser.add_argument("-h", "--help", action="store_true")
-
-parser.add_argument("-v", "--version", action="store_true")
-
-parser.add_argument("commands", type=str, nargs="*")
-
-parser.add_argument("-o", "--outfile", type=str, default="pdfriend_output.pdf")
-
-parser.add_argument("-q", "--quality", type=int, default=100)
 
 def main():
+    parser = argparse.ArgumentParser(add_help=False)
+
+    parser.add_argument("-h", "--help", action="store_true")
+    parser.add_argument("-v", "--version", action="store_true")
+
+    parser.add_argument("commands", type=str, nargs="*")
+
+    parser.add_argument("-o", "--outfile", type=str, default="pdfriend_output.pdf")
+    parser.add_argument("-q", "--quality", type=int, default=100)
+
     args = parser.parse_args()
 
     Platform.Init()
@@ -34,8 +41,30 @@ def main():
         commands.help(command_to_display)
     elif command == "merge":
         if len(args.commands) < 2:
-            raise ValueError(
-                "You need to specify at least one file or pattern to be merged"
-            )
+            print("You need to specify at least one file or pattern to be merged")
+            return
 
         commands.merge(args.commands[1:], args.outfile, args.quality)
+    elif command == "edit":
+        if len(args.commands) < 2:
+            print("You need to specify a file for editing")
+            return
+
+        commands.edit(args.commands[1])
+    elif command == "invert":
+        if len(args.commands) < 2:
+            print("You need to specify a file to be inverted")
+            return
+
+        commands.invert(args.commands[1], args.outfile)
+    elif command == "swap":
+        if len(args.commands) < 4:
+            print("You need to specify a file and 2 pages to be swapped")
+            return
+
+        commands.swap(args.commands[1], args.commands[2], args.commands[3])
+    elif command == "clear":
+        commands.clear()
+    else:
+        print(f"command \"{command}\" not recognized")
+        print("use pdfriend help for a list of the available commands")
