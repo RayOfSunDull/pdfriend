8499909e168e8c43669fb1fadc98ca2844a70e54
RayOfSunDull <apollon.grigoras@gmail.com>
2023-12-28 15:56:19 +0200

    added command parsing functionality

--- pdfriend/classes/cmdparsers.py
+++ pdfriend/classes/cmdparsers.py
@@ -24,7 +24,8 @@ class CmdParser:
         except Exception:
             return default
 
-    def next_typed(self, type_name: str, type_converter):
+    def next_typed(self, type_name: str, type_converter, unless: list[str] | None = None):
+        unless = unless or []
         if len(self.args) == 0:
             raise exceptions.ExpectedError(
                 f"argument {self.current_arg} for command \"{self.cmd_name}\" not provided"
@@ -32,6 +33,10 @@ class CmdParser:
 
         head, tail = self.args[0], self.args[1:]
         self.args = tail
+
+        if head in unless:
+            self.current_arg += 1
+            return head
 
         try:
             result = type_converter(head)
@@ -43,5 +48,21 @@ class CmdParser:
                 f"argument {self.current_arg} for command \"{self.cmd_name}\" (value: {head}) could not be converted to type \"{type_name}\""
             )
 
-    def next_int(self):
-        return self.next_typed("int", int)
+    def next_int(self, unless = None) -> int:
+        return self.next_typed("int", int, unless)
+
+    def next_int_or(self, default: int, unless: list[str] | None = None) -> int:
+        try:
+            return self.next_int(unless)
+        except Exception:
+            return default
+
+    def next_float(self, unless: list[str] | None = None) -> float:
+        return self.next_typed("float", float, unless)
+
+    def next_float_or(self, default: float, unless: list[str] | None = None) -> float:
+        try:
+            return self.next_float(unless)
+        except Exception:
+            return default
+
